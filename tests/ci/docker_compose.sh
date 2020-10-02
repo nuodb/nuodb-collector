32268f7ec36f158d82e9f0f9156ef52649dcc59e
Martin Kysel <mkysel@nuodb.com>
2020-10-02 16:19:55 -0400

    sleeps

--- tests/ci/docker_compose.sh
+++ tests/ci/docker_compose.sh
@@ -6,16 +6,4 @@ docker-compose up&
 
 sleep 120
 
-n=1
-while [ $n -le 30 ]
-do
-  admin_container_id=$(docker container ls -f name=nuoadmin -q)
-  if [[ $admin_container_id ]]; then
-    sleep 10
-    n=$(( n+1 ))
-  else
-    break
-  fi
-done
-
-docker exec "$admin_container_id" nuocmd check database --db-name hockey --check-running --num-processes 2 --timeout 300
+docker exec "$(docker container ls -f name=nuoadmin -q)" nuocmd check database --db-name hockey --check-running --num-processes 2 --timeout 300
