7cd65ebf0ece1d3369f59ac7539ca4f5863fed9d
Martin Kysel <mkysel@nuodb.com>
2020-10-07 13:30:30 -0400

    Change all tests to use await

--- tests/nuodb_test_class.py
+++ tests/nuodb_test_class.py
@@ -17,22 +17,62 @@ def extract_traces(points):
         metrics.add(point["msg_trace_metric"])
     return metrics
 
+
+def assert_await(fxn, timeout=120, interval=10):
+    last_assert = None
+
+    timeout_start = time.time()
+
+    while time.time() < timeout_start + timeout:
+        try:
+            fxn()
+        except AssertionError as e:
+            last_assert = e
+            time.sleep(interval)
+            continue
+        return
+
+    if last_assert:
+        raise last_assert
+
+
 class NuoDBTelegrafTestClass(unittest.TestCase):
     database_name = None
+
+    @classmethod
+    def isDatabaseRunning(cls, client, database):
+        databases = extract_names(client.get_list_database())
+
+        if database not in databases:
+            raise EnvironmentError("%s database not created" % database)
+
+        return True
 
     @classmethod
     def setUpClass(cls):
         # ensure that the database exists
         client = InfluxDBClient('localhost', 8086)
+        assert_await(lambda: cls.isDatabaseRunning(client, cls.database_name), timeout=60)
 
-        timeout = 60  # [seconds]
-        timeout_start = time.time()
+    def assertMeasurementPresent(self, client, measurement):
+        measurements = extract_names(client.get_list_measurements())
 
-        while time.time() < timeout_start + timeout:
-            databases = extract_names(client.get_list_database())
-            if cls.database_name in databases:
-                print "'%s' is up and running" % cls.database_name
-                return
-            time.sleep(1)
+        # verify a random set of stats
+        self.assertIn(measurement, measurements)
 
-        raise EnvironmentError("%s database not created" % cls.database_name)
+    def assertMeasurementCountGt0(self, client, measurement):
+        rs = client.query('select count(*) from %s' % measurement)
+        points = list(rs.get_points(measurement=measurement))
+        self.assertGreater(points[0]["count_raw"], 0)
+
+    def assertTraceInMeasurement(self, client, measurement, trace):
+        rs = client.query("select * from %s" % measurement)
+        points = list(rs.get_points(measurement=measurement))
+        traces = extract_traces(points)
+
+        self.assertIn(trace, traces)
+
+    def assertMeasurementNotEmpty(self, client, measurement):
+        rs = client.query("select * from %s" % measurement)
+        points = list(rs.get_points(measurement=measurement))
+        self.assertGreater(len(points), 0, "Measurement %s does not contain any points" % measurement)
--- tests/stats_test.py
+++ tests/stats_test.py
@@ -4,21 +4,37 @@ from nuodb_test_class import *
 class TestNuoDB(NuoDBTelegrafTestClass):
     database_name = "nuodb"
 
-    def test_measurements(self):
+    def test_measurements_ActualVersion(self):
         client = InfluxDBClient('localhost', 8086)
         client.switch_database(self.database_name)
-        measurements = extract_names(client.get_list_measurements())
+        assert_await(lambda: self.assertMeasurementPresent(client, "ActualVersion"), timeout=120, interval=10)
 
-        # verify a random set of stats
-        self.assertIn("ActualVersion", measurements)
-        self.assertIn("ArchiveQueue", measurements)
-        self.assertIn("ChairmanMigration", measurements)
-        self.assertIn("CurrentActiveTransactions", measurements)
-        self.assertIn("CurrentCommittedTransactions", measurements)
-        self.assertIn("NodeId", measurements)
+    def test_measurements_ArchiveQueue(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "ArchiveQueue"), timeout=120, interval=10)
 
-        # verify that data is being written
-        rs = client.query("select count(*) from CurrentActiveTransactions")
-        points = list(rs.get_points(measurement='CurrentActiveTransactions'))
-        self.assertGreater(points[0]["count_raw"], 0)
+    def test_measurements_ChairmanMigration(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "ChairmanMigration"), timeout=120, interval=10)
 
+    def test_measurements_CurrentActiveTransactions(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "CurrentActiveTransactions"), timeout=120, interval=10)
+
+    def test_measurements_CurrentCommittedTransactions(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "CurrentCommittedTransactions"), timeout=120, interval=10)
+
+    def test_measurements_NodeId(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "NodeId"), timeout=120, interval=10)
+
+    def test_measurements_CurrentActiveTransactions_count(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementCountGt0(client, "CurrentActiveTransactions"), timeout=120, interval=10)
