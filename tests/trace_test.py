f7f6cf0d2aba8cd0f6f4fa237afc37ef7140ee98
Martin Kysel <mkysel@nuodb.com>
2020-10-07 13:15:17 -0400

    change to awaits

--- tests/trace_test.py
+++ tests/trace_test.py
@@ -4,32 +4,34 @@ from nuodb_test_class import *
 class TestNuoDBInternal(NuoDBTelegrafTestClass):
     database_name = "nuodb_internal"
 
-    def test_measurements(self):
+    def test_measurements_nuodb_msgtrace(self):
         client = InfluxDBClient('localhost', 8086)
         client.switch_database(self.database_name)
-        measurements = extract_names(client.get_list_measurements())
+        assert_await(lambda: self.assertMeasurementPresent(client, "nuodb_msgtrace"), timeout=120, interval=10)
 
-        # verify a random set of stats
-        self.assertIn("nuodb_msgtrace", measurements)
-        self.assertIn("nuodb_synctrace", measurements)
-
-    def test_msgtrace(self):
+    def test_measurements_nuodb_synctrace(self):
         client = InfluxDBClient('localhost', 8086)
         client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "nuodb_synctrace"), timeout=120, interval=10)
 
-        rs = client.query("select * from nuodb_msgtrace")
-        points = list(rs.get_points(measurement='nuodb_msgtrace'))
-        traces = extract_traces(points)
-
-        # check that a few msg_traces are being written
-        self.assertIn("TABLE_INDEX_DELETED", traces)
-        self.assertIn("TRANSACTION_START", traces)
-        self.assertIn("TRANSACTION_STATE", traces)
-
-    def test_synctrace(self):
+    def test_msgtrace_TRANSACTION_START(self):
         client = InfluxDBClient('localhost', 8086)
         client.switch_database(self.database_name)
 
-        rs = client.query("select * from nuodb_msgtrace")
-        points = list(rs.get_points(measurement='nuodb_msgtrace'))
-        self.assertGreater(len(points), 0)
+        assert_await(lambda: self.assertMeasurementPresent(client, "nuodb_msgtrace"), timeout=120, interval=10)
+
+        assert_await(lambda: self.assertTraceInMeasurement(client, "nuodb_msgtrace", "TRANSACTION_START"), timeout=120, interval=10)
+
+    def test_msgtrace_TRANSACTION_STATE(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+
+        assert_await(lambda: self.assertMeasurementPresent(client, "nuodb_msgtrace"), timeout=120, interval=10)
+
+        assert_await(lambda: self.assertTraceInMeasurement(client, "nuodb_msgtrace", "TRANSACTION_STATE"), timeout=120, interval=10)
+
+    def test_synctrace_not_empty(self):
+        client = InfluxDBClient('localhost', 8086)
+        client.switch_database(self.database_name)
+        assert_await(lambda: self.assertMeasurementPresent(client, "nuodb_synctrace"), timeout=120, interval=10)
+        assert_await(lambda: self.assertMeasurementNotEmpty(client, "nuodb_synctrace"), timeout=120, interval=10)
