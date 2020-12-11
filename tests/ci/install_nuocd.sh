8e6015e28b42c83fba4c45fd2588eb5b710fab80
Ed Clark <eclark@nuodb.com>
2020-12-11 02:27:01 +0000

    Adjust CI testing to align with new bare metal instructions

--- tests/ci/install_nuocd.sh
+++ tests/ci/install_nuocd.sh
@@ -2,15 +2,18 @@
 
 export INFLUXURL=http://localhost:8086
 export NUOCMD_API_SERVER=http://localhost:8888
+export PYTHON2=python
 
 sudo cp -r nuocd /opt/.
 sudo chmod 777 /opt/nuocd
 pip install -r requirements.txt
 
+sudo systemctl stop telegraf
+
 sudo cp conf/nuodb.conf /etc/telegraf/telegraf.d
 sudo cp conf/outputs.conf /etc/telegraf/telegraf.d
+sudo sed -i.bak "/^\[\[outputs.influxdb]]/s/^/# /" /etc/telegraf/telegraf.conf
 
 sudo systemctl daemon-reload
-sudo systemctl stop telegraf
 
 /usr/bin/telegraf --config /etc/telegraf/telegraf.conf --config-directory /etc/telegraf/telegraf.d&
